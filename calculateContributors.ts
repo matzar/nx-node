a378fb9841597032dbbf7e4677ec51a6a61133ba
Mateusz Zaremba <mat.zar@icloud.com>
2023-12-14 10:34:56 +0000

    feat: calculateContributors, updateReadme

--- calculateContributors.ts
+++ calculateContributors.ts
@@ -1,21 +1,50 @@
-import simpleGit from 'simple-git';
+import simpleGit, { SimpleGit } from 'simple-git';
 import fs from 'fs';
 import path from 'path';
 
 export async function calculateContributors(repositoryPath: string): Promise<number> {
-  const git = simpleGit(repositoryPath);
+  const git: SimpleGit = simpleGit(repositoryPath);
   let contributors = new Map<string, number>();
 
-  // Example logic to list subfolders in 'packages'
   const packagesPath = path.join(repositoryPath, 'packages');
   const subfolders = fs
     .readdirSync(packagesPath)
     .filter((folder) => fs.statSync(path.join(packagesPath, folder)).isDirectory());
 
   for (const folder of subfolders) {
-    // Logic to fetch contributors for each folder
-    // You need to implement this part
+    const folderPath = `packages/${folder}`; // Adjusted path format
+    const log = await git.log([folderPath]);
+    log.all.forEach((commit) => {
+      const contributor = commit.author_name;
+      contributors.set(contributor, (contributors.get(contributor) || 0) + 1);
+    });
   }
 
-  // ... rest of your code
+  let multiProjectContributors = 0;
+  contributors.forEach((count) => {
+    if (count > 1) multiProjectContributors++;
+  });
+
+  await updateReadme(repositoryPath, multiProjectContributors);
+  return multiProjectContributors;
+}
+
+async function updateReadme(repositoryPath: string, count: number): Promise<void> {
+  const readmePath = path.join(repositoryPath, 'README.md');
+  let readmeContent = '';
+
+  if (fs.existsSync(readmePath)) {
+    readmeContent = fs.readFileSync(readmePath, 'utf-8');
+  }
+
+  const metricsSection = `## Contributor Metrics\nNumber of contributors who worked on multiple projects: ${count}\n`;
+
+  // Check if the section already exists and update it, or append a new section
+  if (readmeContent.includes('## Contributor Metrics')) {
+    readmeContent = readmeContent.replace(/## Contributor Metrics[\s\S]*/, metricsSection);
+  } else {
+    readmeContent += metricsSection;
+  }
+
+  fs.writeFileSync(readmePath, readmeContent);
 }
